dea05940fd7754921179938dfd19550e6483a19a
Olaide Hammed <olaskeet@gmail.com>
2023-09-08 04:40:07 -0700

    alx-low_level_programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- Prints Programming is like building a multilingual puzzle
+ *
+ * Return: 0 to end program
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "Hello"
+ *
+ * Return: 0 end of program
+ */
+int main(void)
+{
+	printf("Hello, World!\n");
+	return (0);
+}
